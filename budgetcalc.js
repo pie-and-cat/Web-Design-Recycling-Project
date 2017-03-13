5b2ee8aa2087387dff8da16e8325a7a861845f22
topoisomerase <nancyl1004@gmail.com>
2017-03-13 12:53:00 -0500

    Add files via upload

---
+++ budgetcalc.js
@@ -0,0 +1,28 @@
+function calculator(){
+  //the fundraising
+  var cost = document.getElementById("fundCost").value;
+  var sum = document.getElementById("fundSum").value;
+  var netFund = sum - cost;
+
+  //the pick up (per year)
+  var monthCost = document.getElementById("monthlyCost").value * 1;
+  var monthNumber = document.getElementById("monthNumber").value * 1;
+  var pickUpTotal = monthCost * monthNumber;
+
+  //the bins
+  var binsMed = document.getElementById("binsMed").value * 1; //number of bins
+  var binsSmall = document.getElementById("binsSmall").value * 1;
+  var binsMCost = binsMed * 10; //cost total
+  var binsSCost = binsSmall * 1;
+  var x = 100;
+  var binsCost = x + binsMCost + binsSCost;
+  var binsTotal = 1 + binsMed + binsSmall;
+
+  var remainder = netFund - binsCost - pickUpTotal;
+
+  document.getElementById("output").innerHTML = remainder;
+  document.getElementById("totBins").innerHTML = binsTotal;
+  document.getElementById("totBinsCost").innerHTML = binsCost;
+  document.getElementById("fundNet").innerHTML = netFund;
+  document.getElementById("totPickCost").innerHTML = pickUpTotal;
+}
